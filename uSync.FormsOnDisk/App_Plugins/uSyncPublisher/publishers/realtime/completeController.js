bcb51f14addc4d5e44fff351b881da99cbae5a36
Kevin Jump <kevin@thejumps.co.uk>
2021-10-11 14:11:54 +0100

    Remove v8 sites.

--- uSync.FormsOnDisk/App_Plugins/uSyncPublisher/publishers/realtime/completeController.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    'use strict';
-    function completeController($scope) {
-        $scope.vm.complete = true;
-    }
-
-    angular.module('umbraco')
-        .controller('uSyncPublisherCompleteController', completeController);
-})();
