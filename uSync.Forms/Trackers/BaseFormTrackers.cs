665719fb1b77e8517f8f336f726c21eab46b805e
Kevin Jump <kevin@thejumps.co.uk>
2024-04-22 10:33:38 +0100

    Add form trackers.

---
+++ uSync.Forms/Trackers/BaseFormTrackers.cs
@@ -0,0 +1,34 @@
+﻿using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Models;
+
+using uSync.Core.Serialization;
+using uSync.Core.Tracking;
+
+namespace uSync.Forms.Trackers;
+public class FormTracker : SyncXmlTrackAndMerger<Form>, ISyncTracker<Form>
+{
+	public FormTracker(SyncSerializerCollection serializers) 
+		: base(serializers)
+	{ }
+}
+
+public class FormsFolderTracker : SyncXmlTrackAndMerger<Folder>, ISyncTracker<Folder>
+{
+	public FormsFolderTracker(SyncSerializerCollection serializers)
+		: base(serializers)
+	{ }
+}
+
+public class PreValueTracker : SyncXmlTrackAndMerger<FieldPreValueSource>, ISyncTracker<FieldPreValueSource>
+{
+	public PreValueTracker(SyncSerializerCollection serializers) 
+		: base(serializers)
+	{ }
+}
+
+public class DataSourceTracker : SyncXmlTrackAndMerger<FormDataSource>, ISyncTracker<FormDataSource>
+{
+	public DataSourceTracker(SyncSerializerCollection serializers) 
+		: base(serializers)
+	{ }
+}
