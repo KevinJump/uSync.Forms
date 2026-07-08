ee3598735fae78a576e9ac1bb0fba9ae43414c44
Kevin Jump <kevin@thejumps.co.uk>
2026-07-08 12:09:49 +0100

    usync.forms -  complete package update:

--- uSync.Forms.Complete/wwwroot/App_Plugins/uSyncFormsComplete/u-sync-forms-complete.js
+++ uSync.Forms.Complete/wwwroot/App_Plugins/uSyncFormsComplete/u-sync-forms-complete.js
@@ -1,28 +1,26 @@
-const o = [
-  "forms-form-root",
-  "forms-folder",
-  "forms-form",
-  "forms-prevalue-root",
-  "forms-prevalue",
-  "forms-datasource-root",
-  "forms-datasource"
-], s = [
-  {
-    type: "entityAction",
-    kind: "publisher-push",
-    alias: "usync.forms.push.action",
-    name: "Push Form",
-    forEntityTypes: o
-  },
-  {
-    type: "entityAction",
-    kind: "publisher-pull",
-    alias: "usync.forms.pull.action",
-    name: "Pull Form",
-    forEntityTypes: o
-  }
-], t = [...s], r = [...t];
-export {
-  r as manifests
-};
+//#region src/menus/manifest.ts
+var e = [
+	"forms-form-root",
+	"forms-folder",
+	"forms-form",
+	"forms-prevalue-root",
+	"forms-prevalue",
+	"forms-datasource-root",
+	"forms-datasource"
+], t = [{
+	type: "entityAction",
+	kind: "publisher-push",
+	alias: "usync.forms.push.action",
+	name: "Push Form",
+	forEntityTypes: e
+}, {
+	type: "entityAction",
+	kind: "publisher-pull",
+	alias: "usync.forms.pull.action",
+	name: "Pull Form",
+	forEntityTypes: e
+}];
+//#endregion
+export { t as manifests };
+
 //# sourceMappingURL=u-sync-forms-complete.js.map
